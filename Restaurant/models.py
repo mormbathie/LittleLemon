850c3c7cbc3f704c570ec4a0b77d8d8f6ee6c70f
mor mbathie <mormbathie98@gmail.com>
2023-09-23 12:47:32 +0100

    ajoute des modeles

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BookingTbale(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField()
+class MenuTable(models.Model):
+    Title = models.CharField(max_length=255)
+    price  = models.DecimalField(max_digits=10,decimal_places=2)
+    Inventory = models.IntegerField
